f0d8328a312a107fb8bf8c3e073cbb19054148b6
Fujita <okakilikecat@gmail.com>
2025-05-24 06:28:12 +0900

    ビューの作成

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Genre;
 
 class EventController extends Controller
 {
-    public function index()
+    public function index(int $id)
     {
-        return "Hello world";
+        $genres = Genre::all();
+
+        return view('events/index', [
+            'genres' => $genres,
+            "genre_id" => $id
+        ]);
     }
 }

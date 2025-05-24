bdf59bd8b978115cbd15d5cd60d9b3523da46de1
Fujita <okakilikecat@gmail.com>
2025-05-24 23:40:44 +0900

    eventのモデル作成

---
+++ database/migrations/2025_05_24_134024_create_events_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('genre_id')->unsigned();
+            $table->string('title', 100);
+            $table->date('start_date');
+            $table->integer('status')->default(1);
+            $table->timestamps();
+            $table->foreign('genre_id')->references('id')->on('genres');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
